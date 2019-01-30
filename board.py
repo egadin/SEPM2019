c4e5c7c114c32eb6831a01aecb736e8de4a8fe5d
Erik <godda96@gmail.com>
2019-01-30 11:38:27 +0100

    first 2 turns work with certain inputs, error checking for floats and indexing errors in laypiece and givepiece

--- board.py
+++ board.py
@@ -1,8 +1,8 @@
-from Tkinter import *
+import Tkinter as tk
 import numpy as np
 
-class piece:
-    def __init__(id, shape, color, line, number):
+class Piece:
+    def __init__(self, id, shape, color, line, number):
         self.id = id
         self.shape = shape
         self.color = color
@@ -93,54 +93,92 @@ def GAME_ENDED():
     else:
         return False
 
+
+def givePiece():
+    global turncount
+    global nextPiece
+    for piece in range(0,16-turncount):
+        if (remainingPieces[piece].id == contents.get()):
+            nextPiece = remainingPieces[piece]
+    InstructionEntry.delete(first=0,last=10)
+    turncount =+ 1
+
+def layPiece():
+    global nextPiece
+    global remainingPieces
+    global board
+    column = contents.get() % 4
+    row = (contents.get() // 4) if (contents.get() > 3) else 1
+    InstructionEntry.delete(first=0,last=10)
+    board[row:column] = nextPiece
+    remainingPieces.remove(nextPiece)
+
+
+def EVENT_HANDLER(e):
+    global event
+    if (event == 2):
+        givePiece()
+        event = 1
+        GAME_TURN()
+    elif (event == 1):
+        layPiece()
+        event = 2
+        GAME_TURN()
+    else:
+        quit
+
+
 def GAME_TURN():
-        def givePiece(event):
-            for piece in pieces:
-                if (piece.id == contents.get()):
-                    nextPiece = contents.get()
-            turncount =+ 1
-            GAME_TURN()
+    global event
+    global turncount
+    if (event == 1):
+        PlayerLabel.config(text='Player {}'.format(1 + turncount % 2))
+        InstructionLabel.config(text='Where to place current piece 1-16:')
 
-        def layPiece(event):
-            column = contents.get() % 4
-            row = contents.get() / 4
-            board[row : column] = nextPiece
-            remainingPieces.remove(nextPiece)
-            if (GAME_ENDED() == True):
-                winner = 1 + turncount % 2
-
-        if (turncount == 0):
-            PlayerLabel.config(text='Player {}'.format(1 + turncount % 2))
-            InstructionLabel.config(text='Number of piece to give away 1-16:')
-            InstructionEntry.bind('<Return>', givePiece)
-
-        else:
-            PlayerLabel.config(text='Player {}'.format(1 + turncount % 2))
-            InstructionLabel.config(text='Where to place current piece 1-16:')
-            InstructionEntry.bind('<Return>', layPiece)
-            InstructionLabel.config(text='Number of piece to give away 1-16:')
-            InstructionEntry.bind('<Return>', givePiece)
+    elif (event == 2):
+        PlayerLabel.config(text='Player {}'.format(1 + turncount % 2))
+        InstructionLabel.config(text='Number of piece to give away 1-16:')
 
 
 board = np.empty((4,4,))
 board[:]
-pieces = [[1,"square","black","line",1],[2,"square","black","line",0],[3,"square","black","dotted",1],[4,"square","black","dotted",0],[5,"square","white","line",1],[6,"square","white","line",0],[7,"square","white","dotted",1],[8,"square","white","dotted",0],
-[9,"round","black","line",1],[10,"round","black","line",0],[11,"round","black","dotted",1],[12,"round","black","dotted",0],[13,"round","white","line",1],[14,"round","white","line",0],[15,"round","white","dotted",1],[16,"round","white","dotted",0]]
+pieces = [Piece(1,"square","black","line",1),Piece(2,"square","black","line",0),Piece(3,"square","black","dotted",1),Piece(4,"square","black","dotted",0),Piece(5,"square","white","line",1),Piece(6,"square","white","line",0),Piece(7,"square","white","dotted",1),Piece(8,"square","white","dotted",0),
+Piece(9,"round","black","line",1),Piece(10,"round","black","line",0),Piece(11,"round","black","dotted",1),Piece(12,"round","black","dotted",0),Piece(13,"round","white","line",1),Piece(14,"round","white","line",0),Piece(15,"round","white","dotted",1),Piece(16,"round","white","dotted",0)]
 remainingPieces = pieces
 turncount = 0
-nextPiece = piece
+nextPiece = Piece(0, 0, 0, 0, 0)
+event = 2
 
-mainWindow = Tk()
-canvas = Canvas(mainWindow, bg="white", height=2000, width=2000)
-canvas.pack(side = RIGHT)
-contents = StringVar()
-PlayerLabel = Label(mainWindow, text="")
-PlayerLabel.pack(side = LEFT)
-InstructionLabel = Label(mainWindow, text="")
-InstructionLabel.pack(side = LEFT)
-InstructionEntry = Entry(mainWindow, bd = 5)
+root = tk.Tk()
+root.bind('<Return>', EVENT_HANDLER)
+gameCanvas = tk.Canvas(root, bg="white", height=1031, width=1031)
+gameCanvas.pack()
+
+c1=gameCanvas.create_rectangle(5, 5, 255, 255, fill = 'light grey', width = 5)
+c2=gameCanvas.create_rectangle(262, 5, 512, 255, fill = 'light grey', width = 5)
+c3=gameCanvas.create_rectangle(519, 5, 769, 255, fill = 'light grey', width = 5)
+c4=gameCanvas.create_rectangle(776, 5, 1026, 255, fill = 'light grey', width = 5)
+c5=gameCanvas.create_rectangle(5, 262, 255, 512, fill = 'light grey', width = 5)
+c6=gameCanvas.create_rectangle(262, 262, 512, 512, fill = 'light grey', width = 5)
+c7=gameCanvas.create_rectangle(519, 262, 769, 512, fill = 'light grey', width = 5)
+c8=gameCanvas.create_rectangle(776, 262, 1026, 512, fill = 'light grey', width = 5)
+c9=gameCanvas.create_rectangle(5, 519, 255, 769, fill = 'light grey', width = 5)
+c10=gameCanvas.create_rectangle(262, 519, 512, 769, fill = 'light grey', width = 5)
+c11=gameCanvas.create_rectangle(519, 519, 769, 769, fill = 'light grey', width = 5)
+c12=gameCanvas.create_rectangle(776, 519, 1026, 769, fill = 'light grey', width = 5)
+c13=gameCanvas.create_rectangle(5, 776, 255, 1026, fill = 'light grey', width = 5)
+c14=gameCanvas.create_rectangle(262, 776, 512, 1026, fill = 'light grey', width = 5)
+c15=gameCanvas.create_rectangle(519, 776, 769, 1026, fill = 'light grey', width = 5)
+c16=gameCanvas.create_rectangle(776, 776, 1026, 1026, fill = 'light grey', width = 5)
+contents = tk.IntVar()
+PlayerLabel = tk.Label(root, text="")
+PlayerLabel.pack()
+InstructionLabel = tk.Label(root, text="")
+InstructionLabel.pack()
+InstructionEntry = tk.Entry(root, bd = 5)
 InstructionEntry["textvariable"] = contents
-InstructionEntry.pack(side = RIGHT)
+InstructionEntry.pack()
+
 GAME_TURN()
 
-mainWindow.mainloop()
+root.mainloop()
