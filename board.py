ee96b687e45f7d9782679d3d0b259315892c29b6
Erik <godda96@gmail.com>
2019-02-11 14:18:24 +0100

    working game and remaining pieces, only thing left is to secure user inputs

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 import Tkinter as tk
 import numpy as np
+from PIL import Image, ImageTk
 
 class Game:
     def __init__(self, player1, player2):
@@ -13,6 +14,7 @@ class Game:
         self.player1 = player1
         self.player2 = player2
         self.filledcanvas = []
+        self.indexRemainingPieces = []
 
     def GAME_ENDED(self):
         for row in range(0,4):
@@ -73,11 +75,13 @@ class Game:
         if(self.GAME_ENDED()==True):
             print("ended")
         self.remainingPieces.remove(self.nextPiece)
+        self.remainingPiecesCanvashandler("delete", cont)
         self.pieceCanvas(self.nextPiece.id, cont + 1)
 
     def pieceCanvas(self, id, canvas):
         global boardCanvas
         global imagePaths
+        global imagePoints
         gameCanvas.create_image(imagePoints[canvas-1], image=imagePaths[canvas-1])
 
     def EVENT_HANDLER(self, e):
@@ -102,7 +106,47 @@ class Game:
             PlayerLabel.config(text='{}'.format(self.player1)) if ((1 + self.turncount % 2) == 1) else PlayerLabel.config(text='{}'.format(self.player2))
             InstructionLabel.config(text='Number of piece to give away 1-16:')
 
-
+    def remainingPiecesCanvashandler(self, action, number):
+        global remainingImagePoints
+        global imagePaths
+        global remainingPiecesCanvas
+        if (action == "start"):
+            r1 = remainingPiecesCanvas.create_image(remainingImagePoints[0], image=imagePaths[0])#.resize((60,60), Image.ANTIALIAS))
+            r2 = remainingPiecesCanvas.create_image(remainingImagePoints[1], image=imagePaths[1])#.resize((60,60), Image.ANTIALIAS))
+            r3 = remainingPiecesCanvas.create_image(remainingImagePoints[2], image=imagePaths[2])#.resize((60,60), Image.ANTIALIAS))
+            r4 = remainingPiecesCanvas.create_image(remainingImagePoints[3], image=imagePaths[3])#.resize((60,60), Image.ANTIALIAS))
+            r5 = remainingPiecesCanvas.create_image(remainingImagePoints[4], image=imagePaths[4])#.resize((60,60), Image.ANTIALIAS))
+            r6 = remainingPiecesCanvas.create_image(remainingImagePoints[5], image=imagePaths[5])#.resize((60,60), Image.ANTIALIAS))
+            r7 = remainingPiecesCanvas.create_image(remainingImagePoints[6], image=imagePaths[6])#.resize((60,60), Image.ANTIALIAS))
+            r8 = remainingPiecesCanvas.create_image(remainingImagePoints[7], image=imagePaths[7])#.resize((60,60), Image.ANTIALIAS))
+            r9 = remainingPiecesCanvas.create_image(remainingImagePoints[8], image=imagePaths[8])#.resize((60,60), Image.ANTIALIAS))
+            r10 = remainingPiecesCanvas.create_image(remainingImagePoints[9], image=imagePaths[9])#.resize((60,60), Image.ANTIALIAS))
+            r11 = remainingPiecesCanvas.create_image(remainingImagePoints[10], image=imagePaths[10])#.resize((60,60), Image.ANTIALIAS))
+            r12 = remainingPiecesCanvas.create_image(remainingImagePoints[11], image=imagePaths[11])#.resize((60,60), Image.ANTIALIAS))
+            r13 = remainingPiecesCanvas.create_image(remainingImagePoints[12], image=imagePaths[12])#.resize((60,60), Image.ANTIALIAS))
+            r14 = remainingPiecesCanvas.create_image(remainingImagePoints[13], image=imagePaths[13])#.resize((60,60), Image.ANTIALIAS))
+            r15 = remainingPiecesCanvas.create_image(remainingImagePoints[14], image=imagePaths[14])#.resize((60,60), Image.ANTIALIAS))
+            r16 = remainingPiecesCanvas.create_image(remainingImagePoints[15], image=imagePaths[15])#.resize((60,60), Image.ANTIALIAS))
+            self.indexRemainingPieces.append(r1)
+            self.indexRemainingPieces.append(r2)
+            self.indexRemainingPieces.append(r3)
+            self.indexRemainingPieces.append(r4)
+            self.indexRemainingPieces.append(r5)
+            self.indexRemainingPieces.append(r6)
+            self.indexRemainingPieces.append(r7)
+            self.indexRemainingPieces.append(r8)
+            self.indexRemainingPieces.append(r9)
+            self.indexRemainingPieces.append(r10)
+            self.indexRemainingPieces.append(r11)
+            self.indexRemainingPieces.append(r12)
+            self.indexRemainingPieces.append(r13)
+            self.indexRemainingPieces.append(r14)
+            self.indexRemainingPieces.append(r15)
+            self.indexRemainingPieces.append(r16)
+        elif (action == "delete"):
+            print(number)
+            print(self.indexRemainingPieces)
+            remainingPiecesCanvas.delete(self.indexRemainingPieces[number])
 class Piece:
     def __init__(self, id, shape, color, line, number):
         self.id = id
@@ -173,6 +217,9 @@ root = tk.Tk()
 root.bind('<Return>', tictoc.EVENT_HANDLER)
 gameCanvas = tk.Canvas(root, bg="white", height=1031, width=1031)
 gameCanvas.pack()
+remainingPiecesCanvas = tk.Canvas(root, bg="light grey", height=1031, width=300)
+remainingPiecesCanvas.pack(side = "left")
+remainingImagePoints = [[150,64],[150,128],[150,192],[150,256],[150,320],[150,384],[150,448],[150,512],[150,576],[150,640],[150,704],[150,768],[150,832],[150,896],[150,960],[150,1024]]
 
 c1=gameCanvas.create_rectangle(5, 5, 255, 255, fill = 'light grey', width = 5)
 c2=gameCanvas.create_rectangle(262, 5, 512, 255, fill = 'light grey', width = 5)
@@ -218,7 +265,7 @@ InstructionLabel.config(text='Number of piece to give away 1-16:')
 InstructionEntry = tk.Entry(root, bd = 5)
 InstructionEntry["textvariable"] = contents
 InstructionEntry.pack()
-
+tictoc.remainingPiecesCanvashandler("start",1)
 tictoc.GAME_TURN
 
 root.mainloop()
